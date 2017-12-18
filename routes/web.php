82dee97c0f1d6775d6c847af89b7ebc0450bbc7d
Antonio Giordano <antonio.giordano17@gmail.com>
2017-12-18 09:59:07 +0100

    Route definition

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,7 @@ Route::get('/remarks/{rid}/edit','RemarksController@edit');
 Route::post('/remarks/delete','RemarksController@delete');
 
 Route::post('/remarks/update','RemarksController@update');
+
+
+// Antonio - Entreprises list
+Route::get('/entreprises', 'EntreprisesController@index');
