740fbfb33ccfc7a6f5124c625f2b12dab8021130
Kevin Jordil <kevin.jordil@cpnv.ch>
2017-12-18 10:43:41 +0100

    Add API update view

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,7 @@ Route::get('/wishesMatrix', 'WishesMatrixController@index');
 
 // Kevin
 Route::get('/traveltime', 'TravelTimeController@index');
+Route::post('/traveltime/calculate', 'TravelTimeController@calculate');
 
 // Bastien - Grille d'évaluation
 Route::get('/evalgrid', 'EvalController@index');
