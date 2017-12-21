44fc3bab9f75275ae560132134c0d794a37c3f90
Bastien <bastien.nicoud@gmail.com>
2017-12-21 20:09:18 +0100

    Clean comments EvalController

--- app/Http/Controllers/EvalController.php
+++ app/Http/Controllers/EvalController.php
@@ -1,16 +1,31 @@
 <?php
-// *************************************************
-// Bastien
-// version 1.0
-// EvalController
+/**
+ * EvalController
+ * 
+ * Bastien Nicoud
+ * v1.0.0
+ */
+
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
+/**
+ * EvalController
+ * 
+ * Provides the methods to complete, submit and validate the datas indicated by the user in the evaluation grid.
+ */
 class EvalController extends Controller
 {
-    // index, base route
+    /**
+     * index()
+     *
+     * Returns the Evaluation grid complete wiew.
+     * 
+     * @return view evalGrid/grid
+     */
     public function index()
     {
         return view('evalGrid/grid');
