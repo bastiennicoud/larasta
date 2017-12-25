5fc366e88f01f15f04bd565b17fa7395d8a30d58
Unknown <benjamin.delacombaz@cpnv.ch>
2017-12-25 14:04:00 +0100

    Add wishes matrix clickable case

--- public/js/wishesMatrix.js
+++ public/js/wishesMatrix.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
     $('.clickableCase').click(function(){
-         $(this).append(1);
+        var items=[];
+        var col = $(this).parent().children().index($(this)) + 1;
+        $('tr td:nth-child(' + col + ')').each( function(){
+            //add item to array
+            items.push( $(this).text() );       
+         });
+        
+         if($(this).text() != "")
+         {
+            $(this).text("");
+         }else
+         {
+            // Else if for limit 3 choices
+            if(jQuery.inArray("1",items) == -1)
+            {
+                $(this).text(1)
+            }else if(jQuery.inArray("2",items) == -1)
+            {
+                $(this).text(2)
+            }else if(jQuery.inArray("3",items) == -1)
+            {
+                $(this).text(3)
+            }
+         }
+
     });
  });
