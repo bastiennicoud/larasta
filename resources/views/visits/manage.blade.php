400d987510670045090a09918ffc72e6608b2376
jylecpnv <31761075+jylecpnv@users.noreply.github.com>
2018-01-10 17:57:03 +0100

    Add visits list view, routes and controller
    + Views
    ++ visits : Display all the list
    ++ manage : Display data about a specific visit
    +Controller visits
    ++ Add (test)
    ++ Create (test)
    ++ Mail (WIP)
    ++ Manage (WIP)

--- resources/views/visits/manage.blade.php
+++ resources/views/visits/manage.blade.php
@@ -1,6 +1,66 @@
 @extends ('layout')
 
 @section ('content')
-    <h1>Visits management</h1>
-    <a href="/visits">Go Back</a>
+    <h1>Manage</h1>
+    <button class="btn btn-primary" onclick="mailto()">
+        Envoyer un e-mail
+    </button>
+    <br>
+    <br>
+    <table class="table table-bordered col-md-10">
+        <tr>
+            <th class="col-md-1">Prénom de l'élève</th>
+            <th class="col-md-1">Nom de l'élève</th>
+            <th class="col-md-1">Entreprise</th>
+            <th class="col-md-1">Date de la visite</th>
+            <th class="col-md-1">Heure de la visite</th>
+            <th class="col-md-1">Date de début de stage</th>
+            <th class="col-md-1">Date de fin de stage</th>
+            <th class="col-md-1">email</th>
+        </tr>
+        <tr class="clickable-row text-left" data-href="/remarks/{{ 'bonjour' }}/edit">
+            <td class="col-md-1">{!! $internship->firstname !!}</td>
+            <td class="col-md-1">{!! $internship->lastname !!}</td>
+            <td class="col-md-1">{!! $internship->companyName !!}</td>
+            <td class="col-md-1">{{ (new DateTime($internship->moment))->format('d.m.Y') }}</td>
+            <td class="col-md-1">{{ (new DateTime($internship->moment))->format('H:i:s') }}</td>
+            <td class="col-md-1">{{ (new DateTime($internship->beginDate))->format('d.m.Y') }}</td>
+            <td class="col-md-1">{{ (new DateTime($internship->endDate))->format('d.m.Y') }}</td>
+            <td class="col-md-1"><span class="mailstate">{!! $internship->mailstate !!}</span></td>
+        </tr>
+        <tr>
+            <th colspan="7" class="text-right">Etat de la visite</th>
+            <td>{{ $internship->stateName }}</td>
+            {{$contact->value}}
+        </tr>
+    </table>
+    <form method="post" action="/remarks/add" class="col-md-10 text-left">
+        {{ csrf_field() }}
+        <fieldset>
+            <legend>Ajouter une remarque</legend>
+            <input type="text" name="newremtext"/>
+            <input type="submit" value="Ok"/>
+        </fieldset>
+    </form>
+    </div>
+    <script>
+        //Fonction that open mail app and redirect the user to the main view
+        function mailto()
+        {
+
+            // var url = '/visits/'+{{$internship->internships_id}}+'/mail';
+            // var email = email + {{$contact->value}};
+
+            // var mailto_link = 'mailto:' + email;
+
+            var url = '/visits/'+{{$internship->internships_id}}+'/mail';
+            //var mail = {{$contact->value}};
+
+                location.href = "mailto:jeanyvesle@hotmail.com?subject=Hello world&body=Line one%0DLine two";
+                window.setTimeout(function(){ location.href = url },  1000);
+        }
+    </script>
+@stop
+@section ('page_specific_js')
+    <script src="/js/visit.js"></script>
 @stop
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,9 @@ Route::post('/remarks/delete','RemarksController@delete');
 
 Route::post('/remarks/update','RemarksController@update');
 
+// Quentin N
+Route::get('/contratGen', 'ContratGenController@index');
+
 // Steven
 
 Route::get('/synchro', 'SynchroController@index');
@@ -35,7 +38,13 @@ Route::get('/synchro', 'SynchroController@index');
 // Jean-Yves
 Route::get('/visits','VisitsController@index');
 
-Route::get('/visits/manage','VisitsController@manage');
+Route::get('/visits/{rid}/manage','VisitsController@manage');
+
+Route::get('/visits/add', 'VisitsController@add');
+
+Route::post('/visits/create','VisitsController@create');
+
+Route::get('/visits/{id}/mail','VisitsController@mail');
 
 // Add by Benjamin Delacombaz 12.12.2017 10:40
 Route::get('/wishesMatrix', 'WishesMatrixController@index');
