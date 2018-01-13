5a7591e42acc81f30a6e510d87bd8ec3eb2cdf8b
Quentin Neves <neves.quentin@gmail.com>
2018-01-13 18:49:10 +0100

    Added Tinymce rich text edtior

--- resources/views/contract/contractGender.blade.php
+++ resources/views/contract/contractGender.blade.php
@@ -13,19 +13,12 @@
         Zone d'édition de rich text
         Ne fonctionne pas encore, net::ERR_ARBOTED quand on tente d'accéder au script
         Si on appelle le script après, cela renvoie une erreur 404
-
-    <script src="/vendor/ckeditor/ckeditor/ckeditor.js"/>
-    <form>
-            <textarea name="editor1" id="editor1" rows="10" cols="80">
-                This is my textarea to be replaced with CKEditor.
-            </textarea>
-        <script>
-            // Replace the <textarea id="editor1"> with a CKEditor
-            // instance, using default configuration.
-            CKEDITOR.replace( 'editor1' );
-        </script>
-    </form>
-
     -->
+
+    <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+    <script>tinymce.init({ selector:'textarea' });</script>
+
+    <textarea>Next, start a free trial!</textarea>
+
 
 @stop
