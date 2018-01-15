51f92c7ebcd3d51b583f934e24491b5ed0288b6e
Bastien <bastien.nicoud@gmail.com>
2018-01-15 16:20:29 +0100

    Internship model update

--- app/Internship.php
+++ app/Internship.php
@@ -15,4 +15,36 @@ class Internship extends Model
     {
         return $this->hasMany('App\Visit');
     }
+
+    /**
+     * Relation to retrive the companies
+     */
+    public function companie()
+    {
+        return $this->belongsTo('App\Companies');
+    }
+
+    /**
+     * Relation to retrive the student
+     */
+    public function student()
+    {
+        return $this->belongsTo('App\Persons', 'intern_id');
+    }
+
+    /**
+     * Relation to retrive the teacher
+     */
+    public function teacher()
+    {
+        return $this->belongsTo('App\Persons', 'responsible_id');
+    }
+
+    /**
+     * Relation to retrive the internship master
+     */
+    public function intern()
+    {
+        return $this->belongsTo('App\Persons', 'responsible_id');
+    }
 }
