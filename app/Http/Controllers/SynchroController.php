ebb7825d3423b26601169a106802ea3c224cd521
Steven <steven.avelino@cpnv.ch>
2018-01-19 13:56:46 +0100

    Added code documentation and admin verification
    Documentation for the SynchroController and the IntranetConnection and the check for the role of the user connected to the SynchroController

--- app/Http/Controllers/SynchroController.php
+++ app/Http/Controllers/SynchroController.php
@@ -1,27 +1,51 @@
 <?php
-/*
+/**
  * Title : SynchroController.php
  * Author : Steven Avelino
  * Creation Date : 12 December 2017
- * Modification Date : 15 January 2017
- * Version : 0.3
- * Controller for the Synchronisation between the intranet API and this application
+ * Modification Date : 19 January 2018
+ * Version : 0.4
+ * Controller for the Synchronisation between the intranet API and this application database
 */
 
 namespace App\Http\Controllers;
 
+/**
+ * We use 3 models in this controller
+ * IntranetConnection : Model for the connection to the intranet API. We retrieve the students, teachers and classes.
+ * Persons : Eloquent model for the table "persons" in the MySQL database.
+ * Flock : Eloquent model for the table "flocks" in the MySQL database. 
+*/
 use App\IntranetConnection as Connection;
 use Illuminate\Http\Request;
+use CPNVEnvironment\Environment;
 use App\Persons;
 use App\Flock;
 
 class SynchroController extends Controller
 {
+    /**
+     * Class attributes
+     * $goodPersons : Array that contains the people that are both in the intranet and in the application database.
+     * $obsoletePersons : Array that contains the people in the application database but not in the intranet.
+     * $newPersons : Array that contains the people that were found in the intranet but not in the application database
+     * $classesList : Array that contains all the classes retrieved from the intranet
+    */
     private $goodPersons = [];
     private $obsoletePersons = [];
     private $newPersons = [];
     private $classesList;
 
+    /**
+     * dbObsoletePersons
+     * 
+     * This method is called by the method modify, that take the datas from the synchro view.
+     * It will take a person intranet id, then search for it in the database, then set the "obsolete" field to 1.
+     * 
+     * @param $personIntranetId id from the intranet retrieved from the intranet API
+     * 
+     * @return void
+    */
     protected function dbObsoletePersons($personIntranetId)
     {
         $person = Persons::where('intranetUserId', $personIntranetId);
@@ -30,6 +54,16 @@ class SynchroController extends Controller
         $person->save();
     }
 
+    /**
+     * dbNewPersons
+     * 
+     * This method is called by the method modify, that take the datas from the synchro view.
+     * It will take the index of a person selected and then add it to the database with the informations needed that were retrieved from the intranet.
+     * 
+     * @param $personIndex index in the array of people created from the difference between the intranet API and the application database
+     * 
+     * @return void
+     */
     protected function dbNewPersons($personIndex)
     {
         $person = new Persons;
@@ -38,6 +72,7 @@ class SynchroController extends Controller
         $person->lastname = $this->newPersons[$personIndex]['lastname'];
         $person->upToDateDate = $this->newPersons[$personIndex]['updated_on'];
         $person->intranetUserId = $this->newPersons[$personIndex]['id'];
+        /// The intranet sometime returns 2 different values for the occupation field for students so it handles both
         if ($this->newPersons[$personIndex]['occupation'] == "Elève" || $this->newPersons[$personIndex]['occupation'] == "Eleve")
         {
             $person->role = 0;
@@ -50,11 +85,25 @@ class SynchroController extends Controller
         $person->save();
     }
 
+    /**
+     * dbNewClasses
+     * 
+     * This method is called by the method modify, that take the datas from the synchro view.
+     * Similar to the "dbNewPersons" method, this method will check if the classes in the database exists.
+     * If they don't, it will create them with the same of the class and its start year.
+     * It will update the "persons" table and add a flock_id to the people freshly added.
+     * 
+     * @param $personIndex index in the array of people created from the difference between the intranet API and the application database
+     * 
+     * @return void
+     */
     protected function dbNewClasses($personIndex)
     {
         if (Persons::where('intranetUserId', $this->newPersons[$personIndex]['id'])->where('role', 0)->exists())
         {
+            /// Only need to add the flock_id to students, so role = 0
             $person = Persons::where('intranetUserId', $this->newPersons[$personIndex]['id'])->where('role', 0)->first();
+            /// Split the string returned by the intranet API for the date the person was updated on to get the starting year
             $dateSplit = explode('-', $this->newPersons[$personIndex]['updated_on']);
             $flockId = $this->checkFlock($dateSplit[0], $this->newPersons[$personIndex]['current_class']['link']['name']);
 
@@ -64,6 +113,19 @@ class SynchroController extends Controller
         }
     }
 
+    /**
+     * addFlock
+     * 
+     * Method called in the "checkFlock" method, in case a class needs to be created.
+     * This method will add a new class to the database
+     * the name of the class is the name of the class in reality and the year when the class started.
+     * It takes the class master from the intranet.
+     * 
+     * @param $startYear Year when the class started
+     * @param $className Name of the class
+     * 
+     * @return int
+     */
     public function addFlock($startYear, $className)
     {
         $flock = new Flock;
@@ -87,6 +149,18 @@ class SynchroController extends Controller
         return $flock->id;
     }
 
+    /**
+     * checkFlock
+     * 
+     * Method called in the "dbNewClasses" method.
+     * This method will check if a class for a student exists and will return the id to put in the "flock_id" of the person in the database
+     * If the class doesn't exist, it will call the "addFlock" method to create it.
+     * 
+     * @param $startYear Year when the class started
+     * @param $className Name of the class
+     * 
+     * @return int
+     */
     public function checkFlock($startYear, $className)
     {
         /*$dateSys = date('Y-M-D');
@@ -123,12 +197,28 @@ class SynchroController extends Controller
 
     }
 
+    /**
+     * modify
+     * 
+     * This method will synchronize the database with the intranet.
+     * It takes the different datas from the intranet and the database to put them in the class attributes
+     * Take the request and check which action was asked from the user.
+     * Take the checkboxes that were checked and their index if the action was adding new people and the intranet id if it was deleting people
+     * It called their respective method.
+     * 
+     * @param Request $request
+     * 
+     * @return redirect
+     */
     public function modify(Request $request)
     {
         $this->getDatas();
 
+        /// There are 2 buttons in the view with the name modify with values of either add or delete
+        /// Check the value to know which button was called
         if ($request->modify == "add")
         {
+            /// Request returns the checkboxes that were checked and return it as an array with the array index
             foreach ($request->addCheck as $personIndex)
             {
                 $this->dbNewPersons(intval($personIndex));
@@ -138,18 +228,35 @@ class SynchroController extends Controller
             {
                 $this->dbNewClasses(intval($personIndex));
             }
+
+            $request->session()->flash('status', 'Personnes sélectionées ajoutées');
         }
         else if ($request->modify == "delete")
         {
+            /// Request returns the checkboxes that were checked and return their values which are the intranet user id
+            /// The id is used to find the person in the database and then soft delete it
             foreach ($request->deleteCheck as $personIntranetId)
             {
                 $this->dbObsoletePersons($personIntranetId);
             }
-        }
 
+            $request->session()->flash('status', 'Personnes obsolètes sélectionées supprimées');
+        }
         return redirect('/synchro');
     }
 
+    /**
+     * getDatas
+     * 
+     * This method will get the datas we need from the intranet API or the database.
+     * It will also sort the returned arrays for a better view.
+     * The method will compare what is on the intranet and on the database.
+     * If both the datas on the intranet and the database are similar, it will put these datas in a class attribute called $goodPersons.
+     * If datas are present in the intranet but not in the database, it will put the datas in the class attribute $newPersons.
+     * If datas are in the database but not returned from the intranet, it will put the datas in the class attribute $obsoletePersons.
+     * 
+     * @return void
+     */
     public function getDatas()
     {
         $dbStudents = Persons::all();
@@ -164,6 +271,7 @@ class SynchroController extends Controller
 
         foreach($dbStudents as $student)
         {
+            /// We check if the person exists with the unique intranet user id
             if(in_array($student->intranetUserId, array_column($personsList, 'id')))
             {
                 array_push($this->goodPersons, $student);
@@ -188,10 +296,23 @@ class SynchroController extends Controller
         usort($this->newPersons,function($a,$b) {return strnatcasecmp($a['lastname'],$b['lastname']);});
     }
 
+    /**
+     * index
+     * 
+     * This method is the method called in the route to display the main view of the functionnality.
+     * It will simply call the getDatas method to initialize the connections and get the datas needed to return to the view.
+     * It will then return the view with the 3 main class attributes : $goodPersons, $newPersons, $obsoletePersons.
+     * 
+     * @return view
+     */
     public function index()
     {
-        $this->getDatas();
+        /// Should be at > 0 in a production environment
+        if (Environment::currentUser()->getLevel() < 5)
+        {
+            $this->getDatas();
 
-        return view('synchro/index')->with([ 'goodStudents' => $this->goodPersons, 'obsoleteStudents' => $this->obsoletePersons, 'newStudents' => $this->newPersons]);
+            return view('synchro/index')->with([ 'goodStudents' => $this->goodPersons, 'obsoleteStudents' => $this->obsoletePersons, 'newStudents' => $this->newPersons]);
+        }
     }
 }
--- app/IntranetConnection.php
+++ app/IntranetConnection.php
@@ -3,8 +3,8 @@
  * Title : IntranetConnection.php
  * Author : Steven Avelino
  * Creation Date : 20 December 2017
- * Modification Date : 15 January 2017
- * Version : 0.3
+ * Modification Date : 19 January 2018
+ * Version : 0.4
  * Model to get informations from the intranet API
 */
 
@@ -12,10 +12,24 @@ namespace App;
 
 class IntranetConnection
 {
+    /**
+     * Class attributes
+     * $studentsList : This attribute will contain the list of students returned by the intranet
+     * $teachersList : This attribute will contain the list of teachers returned by the intranet
+     * $classesList : This attribute will contain the list of classes returned by the intranet
+     */
     private $studentsList;
     private $teachersList;
     private $classesList;
 
+    /**
+     * generateSignature
+     * 
+     * This method will take the query parameters and then create a signature after hashing it with MD5 to be later sent to the intranet.
+     * 
+     * @param $params Parameters for the query that will be then requested to the intranet
+     * @return string
+     */
     public function generateSignature($params)
     {
         $queryParams = $params . getenv('API_KEY') . getenv('API_SECRET');
@@ -24,6 +38,18 @@ class IntranetConnection
         return $signature;
     }
 
+    /**
+     * __construct
+     * 
+     * The construct method of the class.
+     * It uses curl to get JSON from the intranet.
+     * The URLs are already addressed, since we know what type of datas we want to get from the intranet.
+     * When the curl request is done, it puts the returned datas in the right attribute and closes the curl connection.
+     * 
+     * 
+     * @param $type The type of datas we want to get (ex. students)
+     * @return array
+     */
     public function __construct($type)
     {
         $connection = curl_init();
@@ -72,16 +98,37 @@ class IntranetConnection
         curl_close($connection);
     }
 
+    /**
+     * getStudents
+     * 
+     * Getter for the attribute $studentsList
+     * 
+     * @return array
+     */
     public function getStudents()
     {
         return $this->studentsList;
     }
 
+    /**
+     * getTeachers
+     * 
+     * Getter for the attribute $teachersList
+     * 
+     * @return array
+     */
     public function getTeachers()
     {
         return $this->teachersList;
     }
 
+    /**
+     * getClasses
+     * 
+     * Getter for the attribute $classesList
+     * 
+     * @return array
+     */
     public function getClasses()
     {
         return $this->classesList;
